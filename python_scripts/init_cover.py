437512f03270d7a9ecebbb48a73621afde7cab50
Julien ARBEY <julien@arbey.fr>
2018-04-07 13:22:11 +0200

    Covers

--- python_scripts/init_cover.py
+++ python_scripts/init_cover.py
@@ -3,12 +3,9 @@ from time import sleep
 import sys
 import RPi.GPIO as GPIO
 
-pin=int(sys.argv[1])
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.OUT)
 
-GPIO.output(pin, 0)
-sleep(0.25)
-GPIO.output(pin, 1);
+for pin in sys.argv[1].split(","):
+	GPIO.setup(int(pin), GPIO.OUT)
+	GPIO.output(int(pin), 1)
